f1821ec9db4d0d473867b368c1e098c0b9964971
yuanzx33033 <yuanzx33033@gmail.com>
2022-08-17 16:36:40 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 . $1
 export PYTHONPATH="."
 gpu_id=0
-cmd="python3 trainer.py --base_output_dir $base_output_dir --path_length $path_length --hidden_size $hidden_size --embedding_size $embedding_size \
+cmd="python3 trainer.py --learning_rate $learning_rate --base_output_dir $base_output_dir --path_length $path_length --hidden_size $hidden_size --embedding_size $embedding_size \
     --batch_size $batch_size --beta $beta --Lambda $Lambda --use_entity_embeddings $use_entity_embeddings --use_cluster_embeddings $use_cluster_embeddings\
     --train_entity_embeddings $train_entity_embeddings --train_relation_embeddings $train_relation_embeddings \
     --data_input_dir $data_input_dir --vocab_dir $vocab_dir --model_load_dir $model_load_dir --load_model $load_model --total_iterations $total_iterations --nell_evaluation $nell_evaluation"
